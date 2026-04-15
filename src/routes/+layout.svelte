18785ba7f343f82b7ea6afadfcdcfdc0fbe2c5ed
LetiziaDellOro <letiziadellorouniversita@gmail.com>
2026-04-15 11:35:23 +0200

    aggiornamento

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 
+	import "$lib/styles/reset.css";
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!-- Google Fonts Bricolage Grotesque -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@12..96,200..800&family=Caprasimo&family=Epilogue:ital,wght@0,100..900;1,100..900&family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Outfit:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+
+<!-- Adobe Fonts Eldwin Trial -->
+ <link rel="preconnect" href="https://use.typekit.net">
+<link rel="stylesheet" href="https://use.typekit.net/yeg3srf.css">
 </svelte:head>
 
 {@render children()}
